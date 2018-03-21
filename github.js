ec4e9affeef5294a434a0b7435f0fe886e15faa2
nixdie <nixdie@yandex.ru>
2018-03-22 02:01:14 +0700

    fetch and display repos

--- github.js
+++ github.js
@@ -2,15 +2,22 @@ class Github {
   constructor(){
     this.client_id = '658aadfdb385d3814dee';
     this.client_secret = '9e37f020bf7bbf0ebe249207f455a88c4f6fd282';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user){
     const profileResponse = await fetch (`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch (`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 
